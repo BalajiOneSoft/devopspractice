fb6adc6896dcff0b756c58793259d17b3411dc80
BalajiOneSoft <balajim2296@gmail.com>
2023-08-31 16:23:04 +0530

    balaji

--- src/main/java/com/myapp/devopsPractice/Devops1.java
+++ src/main/java/com/myapp/devopsPractice/Devops1.java
@@ -1,10 +1,13 @@
 package com.myapp.devopsPractice;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 public class Devops1 {
-	
+	@GetMapping(value="/practice")
 	public String getMsg() {
 		
-		return "this is devops prtactice";
+		return "this is devops practice";
 	}
 
 }
